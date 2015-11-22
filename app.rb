9e8375ffa62442492383f84a43df921e7ff64829
Gary Siu <Gary@untypeable.com>
2015-11-22 13:54:19 +0000

    Added a dash

--- app.rb
+++ app.rb
@@ -3,13 +3,26 @@ require 'sinatra/contrib/all'
 require 'json'
 
 get '/' do
+
+    @recycling = {
+    username: 'Bob',
+    location: 'London',
+    visits: 2,
+    local_average_visits: 1.3,
+    donated_amount: 24.00
+  }
+  
   erb :index
 end
 
-get '/login' do
-  erb :login
-end
+# get '/login' do
+#   erb :login
+# end
 
 get '/login2' do
   erb :login2
+end
+
+get '/map' do
+  erb :map
 end
